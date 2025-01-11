1b41908fe6ed7164518aadb4027ea917b0c4eb0c
ivanreeve <ivanreevelopez@gmail.com>
2025-01-11 11:52:56 +0800

    fix solution

--- ConditionalStatements.js
+++ ConditionalStatements.js
@@ -25,7 +25,7 @@ function readLine() {
 function getGrade(score) {
   let grade;
   // Write your code here
-  if (score > 25 && score <= 30) { } grade = 'A';
+  if (score > 25 && score <= 30) grade = 'A';
   else if (score > 20 && score <= 25) grade = 'B';
   else if (score > 15 && score <= 20) grade = 'C';
   else if (score > 10 && score <= 15) grade = 'D';
